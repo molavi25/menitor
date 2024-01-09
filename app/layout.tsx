3f9a18ebe9d4b1e613962ee3ad4d27ac8b70a16e
Ali Molavi <alimolavi025@gmail.com>
2024-01-09 19:17:37 +0330

    formatted components with prettier and added first shadcn component for test -> <Button />

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,35 @@
+import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
-
-import { cn } from '@/lib/utils';
 
 const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 });
 
-export const metadata: Metadata = {
-  title: 'Menitor',
-  description:
-    'The app for monitor and track your work and life',
-};
-
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}) => {
   return (
     <html lang='en'>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          'min-h-screen bg-background font-sans antialiased bg-black',
           fontSans.variable,
         )}>
         {children}
       </body>
     </html>
   );
-}
+};
+export default RootLayout;
+
+const metadata: Metadata = {
+  title: 'Menitor',
+  description:
+    'The app for monitor and track your work and life',
+};
+export { metadata };
